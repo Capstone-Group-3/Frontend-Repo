beeb1e8deb2b11621615a8c78ae529a0a03ded72
StroudEN <preston.jhowell@gmail.com>
2022-11-30 14:51:00 -0600

    isAdmin check for Nav and ADPanel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({isAdmin}) => {
+
     return (
         <nav>
             <Link to="products">Browse Products</Link>
             <Link to="login">Log In</Link>
             <Link to="profile">Profile</Link>
+            {isAdmin && <Link to="adminpanel">Admin Panel</Link>}
         </nav>
     )
 };
