7858bdd27eca0df4062018aeee2a8440f2aab7d1
emirt <emirtaletovic22@gmail.com>
2022-12-05 12:58:26 -0600

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,7 @@ const Navbar = ({isAdmin}) => {
             <Link to="login">Log In</Link>
             <Link to="profile">Profile</Link>
             {isAdmin && <Link to="adminpanel">Admin Panel</Link>}
+            <Link to="shopcart">🛒</Link>
         </nav>
     )
 };
