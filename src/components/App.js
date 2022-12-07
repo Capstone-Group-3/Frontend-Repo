c5707a22a7bba006ff12d3d384a2b0e18fbee0d1
StroudEN <preston.jhowell@gmail.com>
2022-12-07 02:08:51 -0600

    Styling Admin Panel Test, Login conditional on Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -137,7 +137,7 @@ console.log("the order data: ", pendingOrders);
         <div>
             <div>
                 <h1 className="header">Marketplace App</h1>
-                <Navbar pendingOrdersState={[pendingOrders, setPendingOrders]} isAdmin={isAdmin}/>
+                <Navbar pendingOrdersState={[pendingOrders, setPendingOrders]} isAdmin={isAdmin} loggedIn={loggedIn} currentToken={currentToken}/>
             </div>
             <Outlet context={pageContext}/>
             <Footer />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = ({isAdmin, pendingOrdersState}) => {
+const Navbar = ({isAdmin, pendingOrdersState, loggedIn, currentToken}) => {
     const [pendingOrders, setPendingOrders] = pendingOrdersState
 
     return (
         <nav className="Navbar">
             <Link to="products">Browse Products</Link>
-            <Link to="login">Log In</Link>
+            {!currentToken && <Link to="login">Log In</Link>}
             <Link to="profile">Profile</Link>
             {isAdmin && <Link to="adminpanel">Admin Panel</Link>}
             <Link to="shopcart">Your Cart 🛒{pendingOrders.length}</Link>
